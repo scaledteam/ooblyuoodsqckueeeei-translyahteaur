29023152946e440427eec6ff911581330f6d3625
igor725 <gvaldovigor@gmail.com>
2022-02-01 18:53:29 +0300

    Script updated

--- js/main.js
+++ js/main.js
@@ -1,111 +1,90 @@
-var MAX_AUTOTRANSLATE_COUNT = 200;
+(()=>{
+	const MAX_AUTO_TRANSLATE_LENGTH = 200;
 
-var chars_ubludan = [
-	"qckue","sche","qckh","tsch","yuoo","yeo","scz","eau",
-	"phf","tcs","sch","yee","yeh","yah","zh","ei","oo","а",
-	"b","v","g","d","e","ee","l","m","n","p","r","s","t","''","'"
-];
+	let chars_ubludan = [
+		"qckue","sche","qckh","tsch","yuoo","yeo","scz","eau",
+		"phf","tcs","sch","yee","yeh","yah","zh","ei","oo","a",
+		"b","v","g","d","e","ee","l","m","n","p","r","s","t","''","'"
+	];
+	
+	let chars_ru = [
+		"к","щ","х","ч","ю","ё","з","о","ф","ц",
+		"ш","ы","э","я","ж","й","у","а",
+		"б","в","г","д","е","и","л","м",
+		"н","п","р","с","т","ъ","ь"
+	];
 
-var chars_ru = [
-	"к","щ","х","ч","ю","ё","з","о","ф","ц",
-	"ш","ы","э","я","ж","й","у","а",
-	"б","в","г","д","е","и","л","м",
-	"н","п","р","с","т","ъ","ь"
-];
+	let translateFrom = (str) => {
+		let newstr = '';
+		let pos = 0;
 
-
-function getIndex(array,value){
-	for(var i=0;i<array.length;i++){
-		if(array[i]==value) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-function translateTo(isAuto){
-	var str = $("#input").val();
-	if(isAuto){
-		if(!$("#input").is(':focus')){
-			return;
-		}
-		if(str.length>MAX_AUTOTRANSLATE_COUNT){
-			$("#button").show();
-			return;
-		}else{
-			$("#button").hide();
-		}
-	}
-	var newstr = '';
-	str = str.toLowerCase();
-	for(i=0;i<str.length;i++){
-		var index = getIndex(chars_ru,str[i]);
-		if(index>-1){
-			newstr += chars_ubludan[index];
-		}else{
-			newstr += str[i];
-		}
-	}
-	$("#output").val(newstr);
-}
-
-function translateFrom(isAuto){
-	var str = $("#output").val();
-	var inp = $("#input");
-	if(isAuto){
-		if(!$("#output").is(':focus')){
-			$("#output").data("first",true);
-			return;
-		}else{
-			if($("#output").data("first")){
-				$("#output").data("first",false);
-				$("#input").val("");
+		while(pos < str.length) {
+			let ok = false;
+			for(let i = 0; i < chars_ubludan.length; i++) {
+				let ubludan_char = chars_ubludan[i];
+				if(str.indexOf(ubludan_char, pos) == pos) {
+					pos += ubludan_char.length;
+					newstr += chars_ru[i];
+					ok = true;
+					break;
+				}
 			}
-		}
-		if(inp.val().length>MAX_AUTOTRANSLATE_COUNT){
-			$("#button").show();
-			return;
-		}else{
-			$("#button").hide();
-		}
-	}
-	var newstr = '';
-	var trpos = 0;
-	while(trpos<str.length){
-		var changed = false;
-		for(i=0;i<chars_ubludan.length;i++){
-			var ubludan_char = chars_ubludan[i];
-			var check = str.substr(trpos,ubludan_char.length);
-			if(ubludan_char===check){
-				var index = getIndex(chars_ubludan,ubludan_char);
-				newstr += chars_ru[index];
-				trpos += ubludan_char.length - 1;
-				changed = true;
-				break;
-			}
-		}
-		if(!changed){
-			if(str.charCodeAt(trpos)!=39){
-				newstr += str[trpos];
-				changed = true;
-			}
-			trpos++;
-		}else{
-			trpos++;
-		}
-	}
-	inp.val(newstr);
-}
 
-function translate() {
-	if(!$("#input").val()){
-		translateFrom();
-	}else{
-		translateTo();
-	}
-}
+			if(!ok) newstr += str[pos++];
+		}
 
-$(function(){
-	$("#actionButton").on("click",translate);
-	$("#output").data("first",true);
-});
+		return newstr;
+	}
+	
+	let translateTo = (str) => {
+		let newstr = '';
+
+		for(let i = 0; i < str.length; i++) {
+			let index = chars_ru.indexOf(str[i]);
+			newstr += index < 0 ? str[i] : chars_ubludan[index];
+		}
+
+		return newstr;
+	}
+
+	window.onload = () => {
+		let output_el = document.getElementById("output");
+		let input_el = document.getElementById("input");
+		let checkbox = document.getElementById("atcheck");
+		let autotranslate = true;
+
+		checkbox.addEventListener('change', (ev) => {
+			autotranslate = ev.target.checked;
+		});
+
+		output_el.addEventListener('input', (ev) => {
+			let txt = ev.target.value;
+			if(autotranslate) {
+				if(txt.length > MAX_AUTO_TRANSLATE_LENGTH) {
+					checkbox.disabled = true;
+					autotranslate = false;
+				}
+			} else if(txt.length < MAX_AUTO_TRANSLATE_LENGTH && checkbox.disabled) {
+				checkbox.disabled = false;
+				autotranslate = true;
+			}
+			if(autotranslate) input_el.value = translateFrom(txt);
+			checkbox.checked = autotranslate;
+		});
+
+		input_el.addEventListener('input', (ev) => {
+			let txt = ev.target.value;
+			if(autotranslate) {
+				if(txt.length > MAX_AUTO_TRANSLATE_LENGTH) {
+					checkbox.disabled = true;
+					autotranslate = false;
+				}
+			} else if(txt.length < MAX_AUTO_TRANSLATE_LENGTH && checkbox.disabled) {
+				checkbox.disabled = false;
+				autotranslate = true;
+			}
+			if(autotranslate) output_el.value = translateTo(txt);
+			checkbox.checked = autotranslate;
+		});
+	}
+})();
